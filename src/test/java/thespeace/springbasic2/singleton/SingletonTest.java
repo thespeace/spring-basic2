e2c6399d29aebd1d98eee9e5b20f167d73a06fa5
thespeace_notebook <thespeace1@gmail.com>
2024-02-14 22:34:40 +0900

    FEAT: Spring-Singletone container

--- src/test/java/thespeace/springbasic2/singleton/SingletonTest.java
+++ src/test/java/thespeace/springbasic2/singleton/SingletonTest.java
@@ -3,6 +3,8 @@ package thespeace.springbasic2.singleton;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import thespeace.springbasic2.AppConfig;
 import thespeace.springbasic2.member.MemberService;
 
@@ -52,6 +54,25 @@ public class SingletonTest {
 
         //참고: 싱글톤 패턴을 구현하는 방법은 여러가지가 있다. 여기서는 객체를 미리 생성해두는 가장 단순하고 안전한 방법을 선택했다.
     }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //1. 조회: 호출할 때 마다 같은 객체를 반환
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+        //2. 조회: 호출할 때 마다 같은 객체를 반환
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        //참조값이 같은 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        //memberService1 == memberService2
+        assertThat(memberService1).isSameAs(memberService2);
+    }
 }
 
 /**
@@ -80,4 +101,21 @@ public class SingletonTest {
  *      안티패턴으로 불리기도 한다.
  *
  *   해결방법 : `싱글톤 컨테이너`
+ *
+ *
+ *
+ *  -싱글톤 컨테이너
+ *   스프링 컨테이너는 싱글톤 패턴의 문제점을 해결하면서, 객체 인스턴스를 싱글톤(1개만 생성)으로 관리한다.
+ *   지금까지 우리가 학습한 스프링 빈이 바로 싱글톤으로 관리되는 빈이다.
+ *
+ *   스프링 컨테이너는 싱글턴 패턴을 적용하지 않아도, 객체 인스턴스를 싱글톤으로 관리한다.
+ *      이전에 설명한 컨테이너 생성 과정을 자세히 보자. 컨테이너는 객체를 하나만 생성해서 관리한다.
+ *   스프링 컨테이너는 싱글톤 컨테이너 역할을 한다. 이렇게 싱글톤 객체를 생성하고 관리하는 기능을 싱글톤 레지스 트리라 한다.
+ *   스프링 컨테이너의 이런 기능 덕분에 싱글턴 패턴의 모든 단점을 해결하면서 객체를 싱글톤으로 유지할 수 있다.
+ *      싱글톤 패턴을 위한 지저분한 코드가 들어가지 않아도 된다.
+ *      DIP, OCP, 테스트, private 생성자로 부터 자유롭게 싱글톤을 사용할 수 있다.
+ *   스프링 컨테이너 덕분에 고객의 요청이 올 때 마다 객체를 생성하는 것이 아니라, 이미 만들어진 객체를 공유해서 효율적으로 재사용할 수 있다.
+ *
+ *   참고: 스프링의 기본 빈 등록 방식은 싱글톤(99%)이지만, 싱글톤 방식만 지원하는 것은 아니다.
+ *        요청할 때 마다 새로운 객체를 생성해서 반환하는 기능도 제공한다. 해당 내용은 빈 스코프와 관련이 있다.
  */
