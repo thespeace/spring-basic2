5447534eb033377a5826a3cd28643ae9fa15e07d
thespeace_notebook <thespeace1@gmail.com>
2024-02-17 17:10:07 +0900

    FEAT: Spring-duplicate registration/conflict

--- src/main/java/thespeace/springbasic2/AutoAppConfig.java
+++ src/main/java/thespeace/springbasic2/AutoAppConfig.java
@@ -1,8 +1,11 @@
 package thespeace.springbasic2;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
+import thespeace.springbasic2.member.MemberRepository;
+import thespeace.springbasic2.member.MemoryMemberRepository;
 
 @Configuration
 @ComponentScan(//설정 정보가 없어도 자동으로 스프링 빈을 등록하는 기능.
@@ -12,7 +15,11 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
 
-
+    //스캔 충돌 예시 코드
+//    @Bean(name = "memoryMemberRepository")
+//    public MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 }
 
 /**
@@ -50,4 +57,23 @@ public class AutoAppConfig {
  *          @Configuration : 앞서 보았듯이 스프링 설정 정보로 인식하고, 스프링 빈이 싱글톤을 유지하도록 추가 처리 를 한다.
  *          @Service : 사실 @Service는 특별한 처리를 하지 않는다. 대신 개발자들이 핵심 비즈니스 로직이 여기에 있겠구나 라고 비즈니스 계층을 인식하는데 도움이 된다.
  *      참고: useDefaultFilters 옵션은 기본으로 켜져있는데, 이 옵션을 끄면 기본 스캔 대상들이 제외된다. 알고 넘어가자.
+ *
+ *
+ *
+ *  -중복 등록과 충돌
+ *   컴포넌트 스캔에서 같은 빈 이름을 등록하면 어떻게 될까?
+ *   다음 두가지 상황이 있다.
+ *      1. 자동 빈 등록 vs 자동 빈 등록
+ *          컴포넌트 스캔에 의해 자동으로 스프링 빈이 등록되는데, 그 이름이 같은 경우 스프링은 오류를 발생시킨다.
+ *              ConflictingBeanDefinitionException 예외 발생
+ *      2. 수동 빈 등록 vs 자동 빈 등록
+ *          이 경우 수동 빈 등록이 우선권을 가진다. (수동 빈이 자동 빈을 오버라이딩 해버린다.)
+ *
+ *   수동 빈 등록시 남는 로그
+ *      ```Overriding bean definition for bean 'memoryMemberRepository' with a different definition: replacing```
+ *
+ *      물론 개발자가 의도적으로 이런 결과를 기대했다면, 자동 보다는 수동이 우선권을 가지는 것이 좋다.
+ *      하지만 현실은 개 발자가 의도적으로 설정해서 이런 결과가 만들어지기 보다는 여러 설정들이 꼬여서 이런 결과가 만들어지는 경우가 대부분이다!
+ *      `그러면 정말 잡기 어려운 버그가 만들어진다. 항상 잡기 어려운 버그는 애매한 버그다`.
+ *      그래서 최근 스프링 부트에서는 수동 빈 등록과 자동 빈 등록이 충돌나면 오류가 발생하도록 기본 값을 바꾸었다.
  */
